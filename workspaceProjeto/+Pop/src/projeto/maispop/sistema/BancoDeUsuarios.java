7ce1f61e7d94b9dbff16aa498790eea60ae934e6
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2015-08-30 22:42:56 -0300

    modificacao futil

--- workspaceProjeto/+Pop/src/projeto/maispop/sistema/BancoDeUsuarios.java
+++ workspaceProjeto/+Pop/src/projeto/maispop/sistema/BancoDeUsuarios.java
@@ -13,122 +13,121 @@ import projeto.maispop.usuario.Usuario;
 
 public class BancoDeUsuarios {
 
-    private List<Usuario> usuarios;
+	private List<Usuario> usuarios;
 
-    public BancoDeUsuarios() {
-	this.usuarios = new ArrayList<>();
-    }
-
-    public void cadastraUsuario(String nome, String email, String senha,
-	    String dataNascimento, String imagem) throws EntradaException,
-	    UsuarioExistenteException {
-
-	if (verificaEmailExistente(email)) {
-	    throw new UsuarioExistenteException();
+	public BancoDeUsuarios() {
+		this.usuarios = new ArrayList<>();
 	}
 
-	Usuario novoUsuario = new Usuario(nome, email, senha, dataNascimento,
-		imagem);
-	this.usuarios.add(novoUsuario);
+	public void cadastraUsuario(String nome, String email, String senha,
+			String dataNascimento, String imagem) throws EntradaException,
+			UsuarioExistenteException {
 
-    }
+		if (verificaEmailExistente(email)) {
+			throw new UsuarioExistenteException();
+		}
 
-    public void cadastraUsuario(String nome, String email, String senha,
-	    String dataNascimento) throws EntradaException,
-	    UsuarioExistenteException {
-	if (verificaEmailExistente(email)) {
-	    throw new UsuarioExistenteException();
+		Usuario novoUsuario = new Usuario(nome, email, senha, dataNascimento,
+				imagem);
+		this.usuarios.add(novoUsuario);
+
 	}
 
-	Usuario novoUsuario = new Usuario(nome, email, senha, dataNascimento);
-	this.usuarios.add(novoUsuario);
-    }
+	public void cadastraUsuario(String nome, String email, String senha,
+			String dataNascimento) throws EntradaException,
+			UsuarioExistenteException {
+		if (verificaEmailExistente(email)) {
+			throw new UsuarioExistenteException();
+		}
 
-    public void removeUsuario(String email) throws UsuarioInexistenteException {
-	this.usuarios.remove(getUsuario(email));
-    }
-
-    private boolean verificaEmailExistente(String email) {
-	for (Usuario usuario : usuarios) {
-	    if (usuario.getEmail().equals(email)) {
-		return true;
-	    }
-	}
-	return false;
-    }
-
-    public Usuario getUsuario(String emailUsuario) throws UsuarioInexistenteException{
-	for (Usuario usuario : usuarios) {
-	    if (usuario.getEmail().equals(emailUsuario)) {
-		return usuario;
-	    }
-	}
-	throw new UsuarioInexistenteException("Um usuarix com email " + emailUsuario + " nao esta cadastradx.");
-    }
-
-    public String getInfoUsuario(String atributo, String email)
-	    throws EntradaException, LogicaException{
-	Usuario usuario = getUsuario(email);
-	if (usuario == null) {
-	    throw new UsuarioInexistenteException("Um usuarix com email "
-		    + email + " nao esta cadastradx.");
+		Usuario novoUsuario = new Usuario(nome, email, senha, dataNascimento);
+		this.usuarios.add(novoUsuario);
 	}
 
-	switch (atributo) {
-	case "Nome":
-	    return usuario.getNome();
-	case "Email":
-	    return usuario.getEmail();
-	case "Senha":
-	    throw new RequisicaoException("A senha dx usuarix eh protegida.");
-	case "Data de Nascimento":
-	    return usuario.getDataNascimento();
-	case "Foto":
-	    return usuario.getImagemPerfil();
-	default:
-	    throw new EntradaException();
-	}
-    }
-
-    public void atualizaPerfil(String atributo, String valor,
-	    String antigoValor, String email) throws SenhaException, UsuarioInexistenteException {
-	Usuario usuario = getUsuario(email);
-
-	if (usuario.getSenha().equals(antigoValor)) {
-	    usuario.setSenha(valor);
-	} else {
-	    throw new SenhaException("A senha fornecida esta incorreta.");
-	}
-    }
-
-    public void atualizaPerfil(String atributo, String valor, String email)
-	    throws EntradaException, UsuarioInexistenteException {
-	Usuario usuario = getUsuario(email);
-	if (usuario == null) {
-	    throw new UsuarioInexistenteException("Usuario nao logado.");
+	public void removeUsuario(String email) throws UsuarioInexistenteException {
+		this.usuarios.remove(getUsuario(email));
 	}
 
-	switch (atributo) {
-	case "Nome":
-	    usuario.setNome(valor);
-	    break;
-	case "E-mail":
-	    usuario.setEmail(valor);
-	    break;
-	case "Data de Nascimento":
-	    usuario.setDataNascimento(valor);
-	    break;
-	case "Foto":
-	    usuario.setImagemPerfil(valor);
-	    break;
-	default:
-	    throw new EntradaException();
+	private boolean verificaEmailExistente(String email) {
+		for (Usuario usuario : usuarios) {
+			if (usuario.getEmail().equals(email)) {
+				return true;
+			}
+		}
+		return false;
 	}
-    }
 
-    //RELACIONAMENTO ENTRE USUARIOS:
-    
-    
-    
-    
+	public Usuario getUsuario(String emailUsuario)
+			throws UsuarioInexistenteException {
+		for (Usuario usuario : usuarios) {
+			if (usuario.getEmail().equals(emailUsuario)) {
+				return usuario;
+			}
+		}
+		throw new UsuarioInexistenteException("Um usuarix com email "
+				+ emailUsuario + " nao esta cadastradx.");
+	}
+
+	public String getInfoUsuario(String atributo, String email)
+			throws EntradaException, LogicaException {
+		Usuario usuario = getUsuario(email);
+		if (usuario == null) {
+			throw new UsuarioInexistenteException("Um usuarix com email "
+					+ email + " nao esta cadastradx.");
+		}
+
+		switch (atributo) {
+		case "Nome":
+			return usuario.getNome();
+		case "Email":
+			return usuario.getEmail();
+		case "Senha":
+			throw new RequisicaoException("A senha dx usuarix eh protegida.");
+		case "Data de Nascimento":
+			return usuario.getDataNascimento();
+		case "Foto":
+			return usuario.getImagemPerfil();
+		default:
+			throw new EntradaException();
+		}
+	}
+
+	public void atualizaPerfil(String atributo, String valor,
+			String antigoValor, String email) throws SenhaException,
+			UsuarioInexistenteException {
+		Usuario usuario = getUsuario(email);
+
+		if (usuario.getSenha().equals(antigoValor)) {
+			usuario.setSenha(valor);
+		} else {
+			throw new SenhaException("A senha fornecida esta incorreta.");
+		}
+	}
+
+	public void atualizaPerfil(String atributo, String valor, String email)
+			throws EntradaException, UsuarioInexistenteException {
+		Usuario usuario = getUsuario(email);
+		if (usuario == null) {
+			throw new UsuarioInexistenteException("Usuario nao logado.");
+		}
+
+		switch (atributo) {
+		case "Nome":
+			usuario.setNome(valor);
+			break;
+		case "E-mail":
+			usuario.setEmail(valor);
+			break;
+		case "Data de Nascimento":
+			usuario.setDataNascimento(valor);
+			break;
+		case "Foto":
+			usuario.setImagemPerfil(valor);
+			break;
+		default:
+			throw new EntradaException();
+		}
+	}
+
+	// RELACIONAMENTO ENTRE USUARIOS:
 }
