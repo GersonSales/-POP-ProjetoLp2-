7a2aff7ea710f0a044a08568cdcf1e3ddd181069
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2015-10-16 07:19:06 -0300

    alteracao teste postagem

--- workspaceProjeto/+Pop/src/projeto/maispop/testes/PostagemFormatTeste.java
+++ workspaceProjeto/+Pop/src/projeto/maispop/testes/PostagemFormatTeste.java
@@ -6,25 +6,18 @@ import java.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 import projeto.maispop.excecoes.EntradaException;
-import projeto.maispop.usuario.IconePop;
-import projeto.maispop.usuario.Postagem;
-import projeto.maispop.usuario.TipoUsuario;
 
 public class PostagemFormatTeste {
 
     @Test
     public void test() throws EntradaException {
 
-	Postagem postagemQualquer = new Postagem(
-		"Texto qualquer. <audio caminho</audio>  #teste2",
-		"29/08/2015 04:44:23");
-	TipoUsuario usuarioCelebridade = new IconePop();
 
-	System.out.println(postagemQualquer.getHashTags());
+	//System.out.println(postagemQualquer.getHashTags());
 
-	usuarioCelebridade.descurtir(postagemQualquer);
+	//usuarioCelebridade.descurtir(postagemQualquer);
 
-	System.out.println(postagemQualquer.getHashTags());
+	//System.out.println(postagemQualquer.getHashTags());
 
 	System.out.println("-----Data-----");
 
