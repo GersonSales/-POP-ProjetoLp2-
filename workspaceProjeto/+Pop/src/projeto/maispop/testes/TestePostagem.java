93aabde03695dd40c99ed71c3862c6f3aead24d0
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2015-10-27 09:11:21 -0300

    teste adicionado para debug

--- workspaceProjeto/+Pop/src/projeto/maispop/testes/TestePostagem.java
+++ workspaceProjeto/+Pop/src/projeto/maispop/testes/TestePostagem.java
@@ -1,14 +1,20 @@
 package projeto.maispop.testes;
 
-import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import projeto.maispop.excecoes.EntradaException;
+import projeto.maispop.midia.FabricaPostavel;
 
 public class TestePostagem {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws EntradaException {
+		String teste = "Hoje o sol me acordou. Foi muito cansativo sair da cama pois ainda estava com muito sono. Gostaria ter mais tempo para dormir. Ainda bem que tinha tapioca e cuscuz no cafe da manha para dar energia. #cafe #acorda";
+		try {
+			FabricaPostavel.getListaMidia(teste);
+		}catch(Throwable erro) {
+			System.out.println(erro.getMessage());
+		}
 	}
-
 }
